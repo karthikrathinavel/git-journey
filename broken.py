2606ea463edf7d66cfe5d88314cba4317a527e54
Karthik Rathinavel <80736284+karthikrathinavel@users.noreply.github.com>
2026-06-24 17:36:10 +0530

    Create broken.py

---
+++ broken.py
@@ -0,0 +1,8 @@
+# Print text to the terminal
+print("Hello, World!")
+
+# Get user input and store it in a variable
+user_name = input("Enter your name: ")
+
+# Output an greeting using an f-string
+print(f"Welcome to Python, {user_name}!")
